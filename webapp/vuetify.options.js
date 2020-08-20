3ea8b6182f4d21fc01f71f7b478686623876a2be
Kazi Sadman Ahmed <sadman.bd@gmail.com>
2020-08-20 20:59:27 +0600

    Add initial webapp code

---
+++ webapp/vuetify.options.js
@@ -0,0 +1,13 @@
+export default {
+  theme: {
+    dark: false,
+    themes: {
+      light: {
+        primary: '#fa4d55',
+        secondary: '#F3F3F3',
+        anchor: '#8c9eff',
+        background: '#F3F3F3'
+      }
+    }
+  }
+}
