24b0024c9e32513cc9e92bf6839b8ed3ba567669
Kazi Sadman Ahmed <sadman.bd@gmail.com>
2020-08-23 18:15:34 +0600

    Add register page and update login page

--- webapp/app/pages/auth/register.vue
+++ webapp/app/pages/auth/register.vue
@@ -3,13 +3,15 @@
     v-row(justify="center")
       v-col(cols=12 sm=8 md=6 lg=4)
         v-card
-          v-card-title.headline.grey.lighten-2 Login
+          v-card-title.headline.grey.lighten-2 Register
           v-card-text
             v-container
               v-form
+                v-text-field(v-model="fullName" label="Name" type="text" required)
                 v-text-field(v-model="email" label="Email" type="email" required)
                 v-text-field(v-model="password" label="Password" type="password" required)
-                v-btn(color="primary" type="submit") Login
+                v-text-field(v-model="passwordConfirm" label="Confirm Password" type="password" required)
+                v-btn(color="primary" type="submit") Register
 </template>
 
 <script>
